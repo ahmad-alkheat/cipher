66d4a2f7eebeccd61ea8ee8f102f099114a8f620
Ahmad <wisamfaithful@gmail.com>
2015-04-24 01:23:38 -0400

    refactor tests

--- lib/cipher/keystream.rb
+++ lib/cipher/keystream.rb
@@ -9,22 +9,50 @@ module Cipher
       generate message
     end
 
-    def move_joker_A
-      move_joker 'A',1
+
+    def generate message
+      n = message.gsub(" ","").length
+      while(@key_stream_message.length < n) 
+        move_joker_A 
+        move_joker_B
+        triple_cut
+        count_cut
+        index = find_output_card
+        c = convert_to_letter index
+        @key_stream_message << c if c
+      end
+      @key_stream_message = @key_stream_message.join("").scan(/...../).join(" ")
     end
 
-    def move_joker_B 
-      move_joker 'B',2
-    end
+    
+    private
 
-    def triple_cut
-      first_joker_index = @deck.index { |x| is_joker x }
-      second_joker_index = @deck.index { |x| (is_joker x) && x != @deck[first_joker_index] }
-      first_part = @deck[0..first_joker_index-1]
-      second_part = @deck[second_joker_index + 1.. @deck.length]
-      @deck = @deck[first_joker_index..second_joker_index]
-      @deck = second_part + @deck + first_part
-    end
+      def move_joker(letter, step)
+        new_position = (@deck.index(letter) + step) % @deck.length
+        new_position = 1 if new_position.zero?
+        @deck.insert(new_position,  @deck.delete(letter))
+      end
+
+      def is_joker card
+        card == 'A' || card == 'B'
+      end
+
+      def move_joker_A
+        move_joker 'A',1
+      end
+
+      def move_joker_B 
+        move_joker 'B',2
+      end
+
+      def triple_cut
+        first_joker_index = @deck.index { |x| is_joker x }
+        second_joker_index = @deck.index { |x| (is_joker x) && x != @deck[first_joker_index] }
+        first_part = @deck[0..first_joker_index-1]
+        second_part = @deck[second_joker_index + 1.. @deck.length]
+        @deck = @deck[first_joker_index..second_joker_index]
+        @deck = second_part + @deck + first_part
+      end
 
     def count_cut
       identifier = @deck.last
@@ -45,32 +73,6 @@ module Cipher
       index = index - 26 if index > 26
       Cipher::ALPHABET[index-1]
     end
-
-    def generate message
-      n = message.gsub(" ","").length
-      while(@key_stream_message.length < n) 
-        move_joker_A 
-        move_joker_B
-        triple_cut
-        count_cut
-        index = find_output_card
-        c = convert_to_letter index
-        @key_stream_message << c if c
-      end
-      @key_stream_message = @key_stream_message.join("").scan(/...../).join(" ")
-    end
-
-    private
-
-      def move_joker(letter, step)
-        new_position = (@deck.index(letter) + step) % @deck.length
-        new_position = 1 if new_position.zero?
-        @deck.insert(new_position,  @deck.delete(letter))
-      end
-
-      def is_joker card
-        card == 'A' || card == 'B'
-      end
 
   end
 end
--- spec/keystream_spec.rb
+++ spec/keystream_spec.rb
@@ -10,31 +10,31 @@ describe Cipher::KeyStream do
 
     it 'moves the A joker one step up' do
       @key.deck = [ 1, 2, 3, 'B', 'A' ]
-      expect(@key.move_joker_A).to eq([1,'A',2,3,'B'])
+      expect(@key.send(:move_joker_A)).to eq([1,'A',2,3,'B'])
     end
 
     it 'moves the B joker two steps up' do
       @key.deck = [ 1, 2, 3, 'B', 'A' ]
-      expect(@key.move_joker_B).to eq([ 1, 'B', 2, 3, 'A' ])
+      expect(@key.send(:move_joker_B)).to eq([ 1, 'B', 2, 3, 'A' ])
     end
 
     it 'performs a triple cut around the two jokers' do
       @key.deck = [2, 4, 6, 'B', 5, 8, 7, 1, 'A', 3, 9]
-      expect(@key.triple_cut).to eq([3, 9, 'B', 5, 8, 7, 1, 'A', 2, 4, 6])
+      expect(@key.send(:triple_cut)).to eq([3, 9, 'B', 5, 8, 7, 1, 'A', 2, 4, 6])
     end
 
     it 'performs a count cut' do
       @key.deck = ['A', 7, 2, 'B', 9, 4, 1]
-      expect(@key.count_cut).to eq([7, 2, 'B', 9, 4, 'A', 1])
+      expect(@key.send(:count_cut)).to eq([7, 2, 'B', 9, 4, 'A', 1])
     end
 
     it "outputs the card value that has the top card's index" do
       @key.deck = [2,3,4,7,6]
-      expect(@key.find_output_card).to eq(4) 
+      expect(@key.send(:find_output_card)).to eq(4) 
     end
 
     it 'outputs the next keystream for the message' do
-      expect(@key.convert_to_letter(4)).to eq("D") 
+      expect(@key.send(:convert_to_letter,4)).to eq("D") 
     end
   end
 
