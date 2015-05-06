3470ebc0648e4fd9aaa1e7e9e39766e4dbe32738
Ahmad <wisamfaithful@gmail.com>
2015-05-06 14:38:23 -0400

    encryption method

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -11,8 +11,18 @@ module Cipher
       @message = message
     end
 
+    def encrypt 
+      strip
+      upcase_and_group_in_5
+      stream_message = generate_stream_message
+      original_numbers = to_numbers @message
+      stream_numbers = to_numbers stream_message
+      sum_numbers = add_numbers original_numbers, stream_numbers
+      to_chars sum_numbers
+    end
 
     private 
+
       def strip
         @message = @message.gsub!(/\W+/, '')
       end
@@ -39,9 +49,21 @@ module Cipher
       def add_numbers original, keystream
         sum = []
         (0..original.length-1).each do |i|
-          sum << original[i] + keystream[i]
+          tmp = original[i] + keystream[i] 
+          sum << ( tmp <= 26? tmp : tmp - 26)
         end
         sum
       end
+
+      def to_chars array
+        chars = []
+        array.each do |num|
+          chars << Cipher::ALPHABET[num - 1]
+        end
+        @message = chars.join('').scan(/...../).join(' ')
+      end
+
+
+      
   end
 end
